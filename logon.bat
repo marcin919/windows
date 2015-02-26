fd2e05dbe75da1fde2c8210ea9b84965feccf2df
marcin919 <mar.szmyd@gmail.com>
2015-02-26 11:46:33 +0100

    Create logon.bat

---
+++ logon.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM - Dieses Script verbindet notwendige Laufwerke über die VPN Verbindung.
+
+NET USE P: /DELETE
+NET USE X: /DELETE
+NET USE H: /DELETE
+NET USE P: \\192.168.1.10\project
+NET USE X: \\192.168.1.10\share 
+NET USE H: \\192.168.1.10\user1
