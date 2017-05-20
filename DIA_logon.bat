666a754edbbd748e41f9d8edc40dfdd862695057
marcin919 <mar.szmyd@gmail.com>
2017-05-20 12:10:49 +0200

    Update DIA_logon.bat

--- DIA_logon.bat
+++ DIA_logon.bat
@@ -1,22 +1,31 @@
-pause
-
 @echo Hallo %USERNAME%
 @echo Sie werden jetzt im Netzwerk angemeldet
 @echo =======================================
 
-::Indication
-  net use r: /DELETE
-  net use r: \\SCHWENNINGEN\ET	 		/persistent:no
-
-
 ::DNS-Update
 ipconfig /all > V:\Netc\%COMPUTERNAME%.txt
 
+::LISSI
+  net use k: \\LISSI\dialyse_prog	/persistent:no
+
+::Indication
+  net use r: /DELETE
+  net use r: \\SCHWENNINGEN\ET		 	/persistent:no
+
+::Shares
+  net use f: \\SCHWENNINGEN\bilder     	/persistent:no
+
+  net use p: \\SCHWENNINGEN\Shared	/persistent:no
+  net use q: \\SCHWENNINGEN\Programme	/persistent:no
+  net use s: \\SCHWENNINGEN\worddoku 	/persistent:no
+
+  net use v: \\SCHWENNINGEN\Verteiler   /persistent:no
+
+::Home
+  net use i: \\SCHWENNINGEN\%username% 	/persistent:no
 
 ::Indication Registry Eintrag für alle Benutzer
 regedit /s R:\AAA\indication.reg
 
 ::Intranet Registry Eintrag für alle Benutzer
 regedit /s R:\AAA\intranet.reg
-
-pause
