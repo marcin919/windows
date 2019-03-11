107aa161b568c0d617c159e1d776bceefe337b47
marcin919 <mar.szmyd@gmail.com>
2019-03-11 23:10:40 +0100

    Update and rename EMCO-settings-DE.cmd to EMCO-GDATA_settings-DE.cmd

--- EMCO-GDATA_settings-DE.cmd
+++ EMCO-GDATA_settings-DE.cmd
@@ -9,6 +9,11 @@ Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 136" Dir = in action 
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 137" Dir = in action = allow protocol = UDP localport = 137
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 138" Dir = in action = allow protocol = UDP localport = 138
 
+:: Ports fuer Programm GData welche geöffnet werden sollen, wenn das Windows Firewall eingeschaltet ist.
+:: TCP
+Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 7161" Dir = in action = allow protocol = TCP localport = 7161
+Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 7161" Dir = in action = allow protocol = TCP localport = 7161
+
 :: Datei- und Druckfreigabe (gilt nur für Deutsches Windows)
 netsh advfirewall firewall set rule group = "Datei- und Druckerfreigabe" new enable=Yes
 
