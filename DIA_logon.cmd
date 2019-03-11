c83bac4d84d881afaa516566d273db760ce45797
marcin919 <mar.szmyd@gmail.com>
2019-03-11 21:59:11 +0100

    Update DIA_logon.cmd

--- DIA_logon.cmd
+++ DIA_logon.cmd
@@ -14,18 +14,16 @@ ipconfig /all > V:\adminfreigabe\%COMPUTERNAME%.txt
 
 ::Shares
   net use f: \\SCHWENNINGEN\bilder     	/persistent:no
-
   net use p: \\SCHWENNINGEN\Shared	/persistent:no
   net use q: \\SCHWENNINGEN\Programme	/persistent:no
   net use s: \\SCHWENNINGEN\worddoku 	/persistent:no
-
   net use v: \\SCHWENNINGEN\Verteiler   /persistent:no
 
 ::Home
   net use i: \\SCHWENNINGEN\%username% 	/persistent:no
 
 ::Indication Registry Eintrag für alle Benutzer
-regedit /s R:\AAA\indication.reg
+  regedit /s R:\AAA\indication.reg
 
 ::Intranet Registry Eintrag für alle Benutzer
-regedit /s R:\AAA\intranet.reg
+  regedit /s R:\AAA\intranet.reg
