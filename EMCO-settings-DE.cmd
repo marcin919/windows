621d7f57128fd1d32141d0a2982dd12b6d116eae
marcin919 <mar.szmyd@gmail.com>
2019-03-11 22:04:16 +0100

    Update and rename EMCO-settings-DE.bat to EMCO-settings-DE.cmd

--- EMCO-settings-DE.cmd
+++ EMCO-settings-DE.cmd
@@ -1,16 +1,16 @@
-REM Ports fuer Programm EMCO installer welche geöffnet werden sollen, wenn die Windows Firewall eingeschaltet ist.
-REM TCP
+:: Ports fuer Programm EMCO installer welche geöffnet werden sollen, wenn das Windows Firewall eingeschaltet ist.
+:: TCP
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 135" Dir = in action = allow protocol = TCP localport = 135
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 136" Dir = in action = allow protocol = TCP localport = 136
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 445" Dir = in action = allow protocol = TCP localport = 445
 
-REM UDP
+:: UDP
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 136" Dir = in action = allow protocol = UDP localport = 136
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 137" Dir = in action = allow protocol = UDP localport = 137
 Netsh Advfirewall Firewall add Rule Name = "EMCO Open Port 138" Dir = in action = allow protocol = UDP localport = 138
 
-REM Datei- und Druckfreigabe
+:: Datei- und Druckfreigabe (gilt nur für Deutsches Windows)
 netsh advfirewall firewall set rule group = "Datei- und Druckerfreigabe" new enable=Yes
 
-REM Enable or Disable the Remote Administration Firewall Rule
+:: Enable or Disable the Remote Administration Firewall Rule
 netsh advfirewall firewall set rule group="Remoteverwaltung" new enable=yes
