d5d5384e1ae4cd934fbd888adab64d1bf82d622e
marcin919 <mar.szmyd@gmail.com>
2019-03-11 23:48:58 +0100

    Update MiniBackup.cmd

--- MiniBackup.cmd
+++ MiniBackup.cmd
@@ -1,7 +1,16 @@
-echo Backup gestartet
+
+@echo off
+echo Das Backup wird gestartet...
 REM  Welche Datei von A nach B kopiert werden sollt. 
-REM  Mit D wird überprüft ob die Datei älter ist. 
-REM  Y für überschreiben. 
+REM  /S Kopiert Verzeichnisse und Unterverzeichnisse, die nicht leer sind.
+REM  /E Kopiert alle Unterverzeichnisse (leer oder nicht leer).
+REM  /V Überprüft die Grösse jeder neuen Datei.
+REM  /Y Unterdrückt die Aufforderung zur Bestätigung, dass eine vorhandene Zieldatei überschrieben werden soll.
 REM  Ergebnis wird in Log File gespeichert. 
 
-xcopy "C:\orca_ava" "X:/share/orcabackup" /D /Y 
+xcopy /D /Y "C:\orca_ava" "X:/share/orcabackup"  > X:\share\logs\backuplog.txt
+xcopy /E /S /V /Y T:\Documents\Download U:\Backup\Download\ >> U:\Backup\Backuplog.txt
+xcopy /E /S /V /Y T:\Documents\Musik U:\Backup\Musik\ >> U:\Backup\Backuplog.txt
+
+echo Das Backup wurde ausgefuehrt! Enter zum Beenden...
+pause
