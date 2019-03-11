05abfa9913754245df870add2bb4d5155aec6586
marcin919 <mar.szmyd@gmail.com>
2019-03-11 22:01:10 +0100

    Update DIA_logon.cmd

--- DIA_logon.cmd
+++ DIA_logon.cmd
@@ -6,24 +6,24 @@
 ipconfig /all > V:\adminfreigabe\%COMPUTERNAME%.txt
 
 ::LISSI
-    net use k: \\LISSI\dialyse_prog  /persistent:no
+net use k: \\LISSI\dialyse_prog  /persistent:no
 
 ::Indication
-  net use r: /DELETE
-    net use r: \\SCHWENNINGEN\ET  /persistent:no
+net use r: /DELETE
+net use r: \\SCHWENNINGEN\ET  /persistent:no
 
 ::Shares
-  net use f: \\SCHWENNINGEN\bilder  /persistent:no
-  net use p: \\SCHWENNINGEN\Shared	/persistent:no
-  net use q: \\SCHWENNINGEN\Programme	/persistent:no
-  net use s: \\SCHWENNINGEN\worddoku 	/persistent:no
-  net use v: \\SCHWENNINGEN\Verteiler  /persistent:no
+net use f: \\SCHWENNINGEN\bilder  /persistent:no
+net use p: \\SCHWENNINGEN\Shared	/persistent:no
+net use q: \\SCHWENNINGEN\Programme	/persistent:no
+net use s: \\SCHWENNINGEN\worddoku 	/persistent:no
+net use v: \\SCHWENNINGEN\Verteiler  /persistent:no
 
 ::Home
-  net use i: \\SCHWENNINGEN\%username% 	/persistent:no
+net use i: \\SCHWENNINGEN\%username% 	/persistent:no
 
 ::Indication Registry Eintrag für alle Benutzer
-  regedit /s R:\AAA\indication.reg
+regedit /s R:\AAA\indication.reg
 
 ::Intranet Registry Eintrag für alle Benutzer
-  regedit /s R:\AAA\intranet.reg
+regedit /s R:\AAA\intranet.reg
