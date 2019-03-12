31f409a0f95e7e7f3eeefe4ed4dea97b26f85a8f
marcin919 <mar.szmyd@gmail.com>
2019-03-12 20:54:39 +0100

    Update RDP-mit-SSH-tunneln.cmd

--- RDP-mit-SSH-tunneln.cmd
+++ RDP-mit-SSH-tunneln.cmd
@@ -1,4 +1,5 @@
 @echo off
+
 cd files
 start http://localhost
 echo Bitte schliessen Sie dieses Fenster sobald Sie fertig sind!
@@ -6,5 +7,6 @@ start /b plink.exe -auto_store_key_in_cache -ssh -batch -i private.ppk -N rdptel
 start /b plink.exe -auto_store_key_in_cache -ssh -batch -i private.ppk -N rdptele08@62.222.333.444 -L 10000:192.168.1.246:3389
 start /b plink.exe -auto_store_key_in_cache -ssh -batch -i private.ppk -N rdptele08@62.222.333.444 -L 11000:192.168.1.31:3389
 start /b plink.exe -auto_store_key_in_cache -ssh -batch -i private.ppk -N rdptele08@62.222.333.444 -L 13000:192.168.1.31:3389
+start /b plink.exe -auto_store_key_in_cache -ssh -batch -i private.ppk -N rdptele08@62.222.333.444 -L 13000:192.168.1.80:5900
 
 PAUSE 
