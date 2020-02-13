4860bc430eb7b44049060d4cc5b7db39c48ed78e
marcin919 <mar.szmyd@gmail.com>
2020-02-13 13:24:56 +0100

    Create npackd-installer.cmd

---
+++ npackd-installer.cmd
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul & rem umlaute im Text darstellen
+
+REM 	Windows Software Installation Toolkit powerde by NpackdCL
+REM 	Version 2020-02-12 by marcin919
+
+setlocal enableDelayedExpansion
+
+ncl add -p FirefoxESR
