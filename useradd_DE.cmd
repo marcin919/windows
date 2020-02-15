8f0a1a945bbd15f16946fd6d692a0a5afd0d4c64
marcin919 <mar.szmyd@gmail.com>
2020-02-16 00:48:04 +0100

    Update useradd_DE.cmd

--- useradd_DE.cmd
+++ useradd_DE.cmd
@@ -1,7 +1,7 @@
 echon @off
 
 ::  Lokalen Administrator User für Servicezweke anlegen. 
-net user ServiceAdmin PassXYZ12 /add /active:yes /comment:"Service Administrator" /expires:never /passwordchg:no /passwordreq:yes
+net user ServiceAdmin PassABC123 /add /active:yes /comment:"Service Administrator" /expires:never /passwordchg:no /passwordreq:yes
 
 ::  und in die Administratoren Gruppe einfügen. 
 net localgroup "Administratoren" ServiceAdmin /add
