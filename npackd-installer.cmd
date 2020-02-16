43f165c42ec82facfbfdc9333a9dbd644f9c14be
marcin919 <mar.szmyd@gmail.com>
2020-02-16 12:10:33 +0100

    Delete npackd-installer.cmd

--- npackd-installer.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul & rem umlaute im Text darstellen
-
-REM 	Windows Software Installation Toolkit powerde by NpackdCL
-REM 	Version 2020-02-12 by marcin919
-
-setlocal enableDelayedExpansion
-
-ncl add -p FirefoxESR
