59510f3aa97f91edbe745167a1dc195dfd4fae64
marcin919 <mar.szmyd@gmail.com>
2021-01-25 20:23:32 +0100

    Update clear-firefox.cmd

--- clear-firefox/clear-firefox.cmd
+++ clear-firefox/clear-firefox.cmd
@@ -1,10 +1,15 @@
-
-
-taskkill /im "firefox.exe"
-
 @echo off
+
+REM 25.01.2021
+REM marcin919
+REM Version 1.0 
+REM Clear your all Firefox Cache Data
+REM You can show your Cache Data with 2 Programms MZCookiesView and MZCacheView (Portabel with SyMenu)
+
+REM "Close" Firefox
 taskkill /im "firefox.exe"
-set DataDir=C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\Profiles
-del /q /s /f "%DataDir%"
-rd /s /q "%DataDir%"
+
+set FirefoxDataDir=C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\Profiles
+del /q /s /f "%FirefoxDataDir%"
+rd /s /q "%FirefoxDataDir%"
 for /d %%x in (C:\Users\%USERNAME%\AppData\Roaming\Mozilla\Firefox\Profiles\*) do del /q /s /f %%x\*sqlite
